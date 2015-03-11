4b998ce8528f647d58d8448f8b5a99e58b4eb7b8
figmentbml <beth.jaswa@gmail.com>
2015-03-11 16:35:41 -0600

    search inline edit works

--- app/controllers/rants/search.js
+++ app/controllers/rants/search.js
@@ -7,6 +7,38 @@ export default Ember.ArrayController.extend({
 
   results: function() {
     return this.store.find('rant', { find: this.get('term') });
-  }.property('term')
+  }.property('term'),
+
+  isEditing: false,
+
+  actions: {
+
+    editRant: function(rant) {
+      this.set('isEditing', true);
+    },
+    cancelRant: function(rant) {
+      this.set('isEditing', false);
+      this.transitionToRoute('rant', rant.id);
+    },
+    saveRant: function(rant){
+      var controller = this;
+      var title = this.get('title');
+      var body = this.get('body');
+
+      rant.set('title', title);
+      rant.set('body', body);
+      rant.save().then(function(){
+        this.set('isEditing', false);
+        this.transitionToRoute('rants');
+      }.bind(this));
+    },
+
+    deleteRant: function(rant) {
+      rant.deleteRecord();
+      rant.save().then(function(){
+        this.transitionToRoute('rants');
+      }.bind(this));
+    }
+  }
 
 });
