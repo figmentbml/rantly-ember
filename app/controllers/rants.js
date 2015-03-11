9b6f456443f19fa62ce112e6f89e003948db4fd6
figmentbml <beth.jaswa@gmail.com>
2015-03-11 15:09:59 -0600

    inline editing on index works for rants

--- app/controllers/rants.js
+++ app/controllers/rants.js
@@ -2,12 +2,5 @@ import Ember from "ember";
 
 export default Ember.ArrayController.extend({
 
-  isEditing: false,
 
-  actions: {
-
-    editRant: function(rant) {
-      this.set('isEditing', true);
-    }
-  }
 });
