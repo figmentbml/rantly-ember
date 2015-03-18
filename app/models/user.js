7e0208563a8776f4fe3100efbc0b118ac168b2b4
figmentbml <beth.jaswa@gmail.com>
2015-03-18 11:24:04 -0600

    updated buttons

--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,7 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
-  rants: DS.hasMany('rant')
+  rants: DS.hasMany('rant'),
+  session: DS.hasMany('session')
 
 });
