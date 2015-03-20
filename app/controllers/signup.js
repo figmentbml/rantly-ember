406e8e3f5b59ef547b4bb7c0cde578a59bc4c608
figmentbml <beth.jaswa@gmail.com>
2015-03-20 15:19:05 -0600

    sign up really works now

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -2,31 +2,47 @@ import Ember from "ember";
 
 export default Ember.ArrayController.extend({
 
+  needs: ['application'],
+
   actions: {
 
     register: function() {
       var controller = this;
       var firstName = this.get('fnameCopy'),
           lastName  = this.get('lnameCopy'),
-          email     = this.get('emailCopy').trim(),
-          password  = this.get('password'),
+          email     = this.get('emailCopy'),
+          password  = this.get('passwordCopy'),
           passwordConfirm  = this.get('passwordConfirm');
 
-
-      var user = controller.store.createRecord('user',
-      { firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        password_confirm: passwordConfirm});
-      controller.set('fnameCopy', '');
-      controller.set('lnameCopy', '');
-      controller.set('emailCopy', '');
-      controller.set('passwordCopy', '');
-      controller.set('passwordConfirm', '');
-      user.save().then(function(){
-        controller.transitionToRoute('rants');
-      });
+      if (
+          ((firstName == null) || (firstName.length < 0)) ||
+          ((lastName == null) || (lastName.length < 0)) ||
+          ((email == null) || (email.length < 0)) ||
+          ((password == null) || (password.length < 0)) ||
+          ((passwordConfirm == null) || (passwordConfirm.length < 0)) ||
+          (password !== passwordConfirm)
+        ) {
+        var input = document.getElementsByClassName("errors")[0];
+        var error = document.createTextNode("All fields are required!");
+        input.appendChild(error);
+      } else {
+        var user = controller.store.createRecord('user',
+        { firstName: firstName,
+          lastName: lastName,
+          email: email.trim(),
+          password: password,
+          password_confirmation: passwordConfirm
+        });
+          user.save().then(function(){
+            controller.set('fnameCopy', '');
+            controller.set('lnameCopy', '');
+            controller.set('emailCopy', '');
+            controller.set('passwordCopy', '');
+            controller.set('passwordConfirm', '');
+            controller.transitionToRoute('rants');
+          });
     }
+  },
   }
+
 });
--- app/models/user.js
+++ app/models/user.js
@@ -4,6 +4,8 @@ export default DS.Model.extend({
   email: DS.attr('string'),
   firstName: DS.attr('string'),
   lastName: DS.attr('string'),
+  password: DS.attr('string'),
+  passwordConfirmation: DS.attr('string'),
   rants: DS.hasMany('rant'),
   session: DS.hasMany('session')
 
