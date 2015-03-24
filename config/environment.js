094d180b6de2d427e6839abd6856badce1b30025
figmentbml <beth.jaswa@gmail.com>
2015-03-24 12:59:43 -0600

    Empty commit for Heroku deployment

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'rantly-ember',
     environment: environment,
     baseURL: '/',
-    // adapterURL: process.env.ADAPTER_URL,
+    adapterURL: process.env.ADAPTER_URL,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
